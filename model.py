f9dc524ee70efd9f3fdfe0a47335559071637b44
Ramin <Ramin Toosi>
2024-11-27 11:40:46 +0330

    model modules

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import torch
-from torch import Tensor
+from torch import Tensor, nn
 
 class DiffusionForwardProcess:
     """
@@ -98,3 +98,239 @@ class DiffusionReverseProcess:
             z = torch.randn(xt.shape).to(xt.device)
 
             return mean + sigma * z, x0
+
+
+def get_time_embedding(time_steps: torch.Tensor, t_emb_dim: int) -> torch.Tensor:
+    """
+    Transform a scalar time-step into a vector representation of size t_emb_dim.
+    :param time_steps: 1D tensor of size -> (Batch, )
+    :param t_emb_dim: Embedding Dimension -> for ex: 128 (scalar value)
+    :return tensor of size -> (B, t_emb_dim)
+    """
+
+    assert t_emb_dim % 2 == 0, "time embedding must be divisible by 2."
+
+    factor = 2 * torch.arange(start=0,
+                              end=t_emb_dim // 2,
+                              dtype=torch.float32,
+                              device=time_steps.device
+                              ) / t_emb_dim
+
+    factor = 10000 ** factor
+
+    t_emb = time_steps[:, None]  # B -> (B, 1)
+    t_emb = t_emb / factor  # (B, 1) -> (B, t_emb_dim//2)
+    t_emb = torch.cat([torch.sin(t_emb), torch.cos(t_emb)], dim=1)  # (B , t_emb_dim)
+
+    return t_emb
+
+
+class NormActConv(nn.Module):
+    """
+    Perform GroupNorm, Activation, and Convolution operations.
+    """
+
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 num_groups: int = 8,
+                 kernel_size: int = 3,
+                 norm: bool = True,
+                 act: bool = True
+                 ):
+        super().__init__()
+
+        # GroupNorm
+        self.g_norm = nn.GroupNorm(
+            num_groups,
+            in_channels
+        ) if norm is True else nn.Identity()
+
+        # Activation
+        self.act = nn.SiLU() if act is True else nn.Identity()
+
+        # Convolution
+        self.conv = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size,
+            padding=(kernel_size - 1) // 2
+        )
+
+    def forward(self, x):
+        x = self.g_norm(x)
+        x = self.act(x)
+        x = self.conv(x)
+        return x
+
+
+class TimeEmbedding(nn.Module):
+    """
+    Maps the Time Embedding to the Required output Dimension.
+    """
+
+    def __init__(self,
+                 n_out: int,  # Output Dimension
+                 t_emb_dim: int = 128  # Time Embedding Dimension
+                 ):
+        super().__init__()
+
+        # Time Embedding Block
+        self.te_block = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(t_emb_dim, n_out)
+        )
+
+    def forward(self, x):
+        return self.te_block(x)
+
+
+
+class SelfAttentionBlock(nn.Module):
+    """
+    Perform GroupNorm and Multi-headed Self Attention operation.
+    """
+
+    def __init__(self,
+                 num_channels: int,
+                 num_groups: int = 8,
+                 num_heads: int = 4,
+                 norm: bool = True
+                 ):
+        super().__init__()
+
+        # GroupNorm
+        self.g_norm = nn.GroupNorm(
+            num_groups,
+            num_channels
+        ) if norm is True else nn.Identity()
+
+        # Self-Attention
+        self.attn = nn.MultiheadAttention(
+            num_channels,
+            num_heads,
+            batch_first=True
+        )
+
+    def forward(self, x):
+        batch_size, channels, h, w = x.shape
+        x = x.reshape(batch_size, channels, h * w)
+        x = self.g_norm(x)
+        x = x.transpose(1, 2)
+        x, _ = self.attn(x, x, x)
+        x = x.transpose(1, 2).reshape(batch_size, channels, h, w)
+        return x
+
+
+class Downsample(nn.Module):
+    """
+    Perform Down sampling by the factor of k across Height and Width.
+    """
+
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 k: int = 2,  # Downsampling factor
+                 use_conv: bool = True,  # If Downsampling using conv-block
+                 use_mpool: bool = True  # If Downsampling using max-pool
+                 ):
+        super(Downsample, self).__init__()
+
+        self.use_conv = use_conv
+        self.use_mpool = use_mpool
+
+        # Downsampling using Convolution
+        self.cv = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels, kernel_size=1),
+            nn.Conv2d(
+                in_channels,
+                out_channels // 2 if use_mpool else out_channels,
+                kernel_size=4,
+                stride=k,
+                padding=1
+            )
+        ) if use_conv else nn.Identity()
+
+        # Downsampling using Maxpool
+        self.mpool = nn.Sequential(
+            nn.MaxPool2d(k, k),
+            nn.Conv2d(
+                in_channels,
+                out_channels // 2 if use_conv else out_channels,
+                kernel_size=1,
+                stride=1,
+                padding=0
+            )
+        ) if use_mpool else nn.Identity()
+
+    def forward(self, x):
+
+        if not self.use_conv:
+            return self.mpool(x)
+
+        if not self.use_mpool:
+            return self.cv(x)
+
+        return torch.cat([self.cv(x), self.mpool(x)], dim=1)
+
+
+class Upsample(nn.Module):
+    """
+    Perform Upsampling by the factor of k across Height and Width
+    """
+
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 k: int = 2,  # Upsampling factor
+                 use_conv: bool = True,  # Upsampling using conv-block
+                 use_upsample: bool = True  # Upsampling using nn.upsample
+                 ):
+        super().__init__()
+
+        self.use_conv = use_conv
+        self.use_upsample = use_upsample
+
+        # Upsampling using conv
+        self.cv = nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels,
+                out_channels // 2 if use_upsample else out_channels,
+                kernel_size=4,
+                stride=k,
+                padding=1
+            ),
+            nn.Conv2d(
+                out_channels // 2 if use_upsample else out_channels,
+                out_channels // 2 if use_upsample else out_channels,
+                kernel_size=1,
+                stride=1,
+                padding=0
+            )
+        ) if use_conv else nn.Identity()
+
+        # Upsamling using nn.Upsample
+        self.up = nn.Sequential(
+            nn.Upsample(
+                scale_factor=k,
+                mode='bilinear',
+                align_corners=False
+            ),
+            nn.Conv2d(
+                in_channels,
+                out_channels // 2 if use_conv else out_channels,
+                kernel_size=1,
+                stride=1,
+                padding=0
+            )
+        ) if use_upsample else nn.Identity()
+
+    def forward(self, x):
+
+        if not self.use_conv:
+            return self.up(x)
+
+        if not self.use_upsample:
+            return self.cv(x)
+
+        return torch.cat([self.cv(x), self.up(x)], dim=1)
