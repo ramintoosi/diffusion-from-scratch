a960dff51a703375e08e16e01fe2ec0e4731ebf5
Ramin <Ramin Toosi>
2024-11-27 11:58:13 +0330

    config

---
+++ config.py
@@ -0,0 +1,9 @@
+class CONFIG:
+    model_path = 'ddpm_unet_100.pth'
+    num_epochs = 50
+    lr = 1e-4
+    num_timesteps = 1000
+    batch_size = 32
+    img_size = 48
+    in_channels = 3
+    num_img_to_generate = 1
